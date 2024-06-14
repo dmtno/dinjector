186adcc371840ed5bd9a337a42426c8a7d3c4bfb
Metanoia1 <meta3479@gmail.com>
2024-06-14 12:36:17 -0600

    from dinjectorr.injector import Injector, inject

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dinjectorr',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     description='A simple dependency injector for Python',
     long_description=open('README.md').read(),
