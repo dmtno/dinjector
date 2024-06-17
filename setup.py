848dcbf071c231c35e1ecf8c322d25c9679315f0
dmtno <meta3479@gmail.com>
2024-06-17 13:12:36 -0600

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,6 @@ setup(
     author="DMYTRO IVANOV",
     author_email="dima.ivanov.py@gmail.com",
     description="A simple dependency injector for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
